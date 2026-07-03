90e8173a57b87ea04ae0a2ad5526c29b8bc8ec80
DannyEdelbroek <334855@student.mboutrecht.nl>
2026-07-03 12:27:01 +0200

    feat: refactor BehandelingController to use dependency injection and enhance index method with pagination and filtering

--- app/Http/Controllers/BehandelingController.php
+++ app/Http/Controllers/BehandelingController.php
@@ -2,14 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Behandeling;
 use Illuminate\Http\Request;
 
 use Illuminate\View\View;
 
 class BehandelingController extends Controller
 {
-    public function index(): View
+    
+    protected $behandelingModel;    
+
+    public function __construct(Behandeling $behandelingModel)
     {
-        return view('admin.behandelingen');
+        $this->behandelingModel = $behandelingModel;
+    }
+
+    public function index(Request $request): View
+    {
+        $query = $this->behandelingModel
+            ->newQuery()
+            ->withCount('behandelingPerVoorraad as aantal_producten');
+
+        if ($request->filled('soort')) {
+            $query->where('Naam', $request->soort);
+        }
+
+        $behandelingen = $query->paginate(4)->withQueryString();
+
+        return view('admin.behandelingen', compact('behandelingen'));
     }
 }
