e3fd51d31fda033408fbc95e1b7ec2c852f36c78
Andrei Gogan <andrei.gogan1@gmail.com>
2026-07-03 11:14:20 +0200

    chore(database): update migration wrapper to seed users before createscript

--- database/migrations/2026_07_03_000000_run_createscript_and_stored_procedures.php
+++ database/migrations/2026_07_03_000000_run_createscript_and_stored_procedures.php
@@ -11,6 +11,39 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // Seed users first so foreign key constraints in createscript.sql are satisfied
+        $users = [
+            ['id' => 1, 'name' => 'Salon Eigenaar', 'email' => 'eigenaar@kniplokettiko.nl', 'role' => 'eigenaar'],
+            ['id' => 2, 'name' => 'Fatima El Amrani', 'email' => 'fatima@kniplokettiko.nl', 'role' => 'medewerker'],
+            ['id' => 3, 'name' => 'Sanne de Vries', 'email' => 'sanne.devries@kniplokettiko.nl', 'role' => 'medewerker'],
+            ['id' => 4, 'name' => 'Mohamed El Idrissi', 'email' => 'mohamed.elidrissi@kniplokettiko.nl', 'role' => 'medewerker'],
+            ['id' => 5, 'name' => 'Lisa van Dijk', 'email' => 'lisa.vandijk@kniplokettiko.nl', 'role' => 'medewerker'],
+            ['id' => 6, 'name' => 'Youssef Benali', 'email' => 'youssef.benali@kniplokettiko.nl', 'role' => 'medewerker'],
+            ['id' => 7, 'name' => 'Noor Bakker', 'email' => 'noor.bakker@kniplokettiko.nl', 'role' => 'medewerker'],
+            ['id' => 8, 'name' => 'Kevin Smit', 'email' => 'kevin.smit@kniplokettiko.nl', 'role' => 'medewerker'],
+            ['id' => 9, 'name' => 'Aylin Demir', 'email' => 'aylin.demir@kniplokettiko.nl', 'role' => 'medewerker'],
+            ['id' => 10, 'name' => 'Tom Verhoeven', 'email' => 'tom.verhoeven@kniplokettiko.nl', 'role' => 'medewerker'],
+            ['id' => 11, 'name' => 'Romy Jacobs', 'email' => 'romy.jacobs@kniplokettiko.nl', 'role' => 'medewerker'],
+            ['id' => 12, 'name' => 'Piet van Loenen', 'email' => 'piet.van.loenen@gmail.com', 'role' => 'klant'],
+            ['id' => 13, 'name' => 'Jan Jansen', 'email' => 'jan.jansen@outlook.com', 'role' => 'klant'],
+            ['id' => 14, 'name' => 'Saskia de Boer', 'email' => 'saskia.deboer@yahoo.com', 'role' => 'klant'],
+            ['id' => 15, 'name' => 'Ahmed Mansouri', 'email' => 'ahmed.mansouri@icloud.com', 'role' => 'klant'],
+            ['id' => 16, 'name' => 'Marieke van den Berg', 'email' => 'marieke.vandenberg@ziggo.nl', 'role' => 'klant'],
+            ['id' => 17, 'name' => 'Daan Visser', 'email' => 'daan.visser@live.nl', 'role' => 'klant'],
+        ];
+
+        foreach ($users as $u) {
+            DB::table('users')->insertOrIgnore([
+                'id' => $u['id'],
+                'name' => $u['name'],
+                'email' => $u['email'],
+                'role' => $u['role'],
+                'password' => bcrypt('password'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+
         // 1. Run database/createscript.sql
         $createscriptPath = base_path('database/createscript.sql');
         if (file_exists($createscriptPath)) {
