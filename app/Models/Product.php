d345b9b9ca43f54638941e3e68cf71e4d4768dfd
DannyEdelbroek <334855@student.mboutrecht.nl>
2026-07-03 14:48:26 +0200

    feat: implement Product model with methods for price update and product retrieval

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,65 @@
 
 namespace App\Models;
 
+use DB;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
 
 class Product extends Model
 {
-    //
+    protected $table = 'Product';
+    protected $primaryKey = 'Id';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'Id',
+        'CategorieId',
+        'Naam',
+        'Omschrijving',
+        'Merk',
+        'EANcode',
+        'Houdbaarheidsdatum',
+        'InkoopPrijs',
+        'VerkoopPrijs',
+        'IsActief',
+    ];
+
+    protected $casts = [
+        'IsActief' => 'boolean',
+        'InkoopPrijs' => 'decimal:2',
+        'VerkoopPrijs' => 'decimal:2',
+    ];
+
+    public static function updatePrice(int $id, float $newPrice)
+    {
+        try {
+            return DB::statement('CALL UpdateProductVerkoopprijs(?, ?)', [$id, $newPrice]);
+        } catch (\Exception $e) {
+            Log::error('Error in UpdateProductVerkoopprijs: '.$e->getMessage());
+
+            throw $e;
+        }
+    }
+
+    public static function getProductenByBehandeling(int $id): array
+    {
+        try {
+            return DB::select('CALL GetProductenByBehandeling(?)', [$id]) ?? [];
+        } catch (\Exception $e) {
+            Log::error('Error in GetProductenByBehandeling: '.$e->getMessage());
+
+            return [];
+        }
+    }
+
+    public static function getProductDetail(int $id): array
+    {
+        try {
+            return DB::select('CALL GetProductDetail(?)', [$id]) ?? [];
+        } catch (\Exception $e) {
+            Log::error('Error in GetProductDetail: '.$e->getMessage());
+
+            return [];
+        }
+    }
 }
