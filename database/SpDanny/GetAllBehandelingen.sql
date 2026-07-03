17ded27f455ba109d69c6cedc87463a4c641f2e6
DannyEdelbroek <334855@student.mboutrecht.nl>
2026-07-03 12:27:33 +0200

    feat: add stored procedure GetBehandelingenOverzicht for retrieving active treatments overview

---
+++ database/SpDanny/GetAllBehandelingen.sql
@@ -0,0 +1,23 @@
+USE kapsalon_db;
+
+DELIMITER //
+
+CREATE PROCEDURE GetBehandelingenOverzicht()
+BEGIN
+    SELECT 
+        b.Naam AS Soort,
+        b.Omschrijving,
+        CONCAT(b.Duurminuten, ' min') AS Duur,
+        CONCAT('EUR ', FORMAT(b.Prijs, 2)) AS Prijs,
+        COUNT(bpv.VoorraadId) AS AantalProducten
+    FROM 
+        Behandeling b
+    LEFT JOIN 
+        BehandelingPerVoorraad bpv ON b.Id = bpv.BehandelingId AND bpv.IsActief = 1
+    WHERE 
+        b.IsActief = 1
+    GROUP BY 
+        b.Id, b.Naam, b.Omschrijving, b.Duurminuten, b.Prijs;
+END //
+
+DELIMITER ;
